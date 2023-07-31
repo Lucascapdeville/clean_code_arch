b40f48b7179fa704527be16d8a3172158db222d6
Lucascapdeville <108835745+Lucascapdeville@users.noreply.github.com>
2023-07-31 17:04:03 -0300

    Create email class

---
+++ cccat12_1-master/backend/ride/src/User(domain)/Email.ts
@@ -0,0 +1,20 @@
+import e from "express";
+
+export class Email{
+    address : string;
+
+    constructor(address:string){
+        if (this.validate_email(address)){
+            this.address = address
+        }
+        else{
+            throw new Error('Email não valido')
+        }
+    }
+
+    validate_email(address:string){
+        const validEmailRegex = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        console.log(validEmailRegex.test(address))
+        return validEmailRegex.test(address)
+    }
+}
