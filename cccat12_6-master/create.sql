2344a7a7af0e7c0e69044e3e4e12bf9492e34846
Lucascapdeville <108835745+Lucascapdeville@users.noreply.github.com>
2023-08-07 09:06:37 -0300

    Innital commit
    Import 12_6 folder

---
+++ cccat12_6-master/create.sql
@@ -0,0 +1,33 @@
+drop schema cccat12 cascade;
+create schema cccat12;
+
+create table cccat12.passenger (
+	passenger_id uuid primary key,
+	name text,
+	email text,
+	document text
+);
+
+create table cccat12.driver (
+	driver_id uuid primary key,
+	name text,
+	email text,
+	document text,
+	car_plate text
+);
+
+create table cccat12.ride (
+	ride_id uuid primary key,
+	passenger_id uuid,
+	driver_id uuid,
+	from_lat numeric,
+	from_long numeric,
+	to_lat numeric,
+	to_long numeric,
+	status text,
+	request_date timestamp,
+	accept_date timestamp,
+	start_date timestamp,
+	end_date timestamp,
+	price numeric
+);
